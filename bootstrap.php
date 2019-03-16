4b60a9a4cf5892b5bb4e64a94842de6ffe99761e
Pradeep Kumar <webreinvent@gmail.com>
2019-03-16 14:04:00 +0530

    bootstrap file included

---
+++ bootstrap.php
@@ -0,0 +1,47 @@
+<?php
+require_once(__DIR__.'../../../../wp-config.php');
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
+use Illuminate\View\ViewServiceProvider;
+use Illuminate\Html\HtmlServiceProvider;
+
+/*$app['config'] = $app->share(function () {
+    return Yaml::parse(file_get_contents(__DIR__.'/app/config.yml'));
+});*/
+
+$db = [
+    'driver' => 'mysql',
+    'host' => DB_HOST,
+    //'port' => DB_PORT,
+    'database' => DB_NAME,
+    'username' => DB_USER,
+    'password' => DB_PASSWORD,
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+];
+
+$capsule = new Capsule;
+$capsule->addConnection($db);
+
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+//Set Pagination Variable
+$inputs = Request::capture()->all();
+
+if(isset($inputs['page']))
+{
+    Paginator::currentPageResolver(function () use ($inputs) {
+        return $inputs['page'];
+    });
+}
+
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
--- vaah.php
+++ vaah.php
@@ -13,55 +13,8 @@ include_once(ABSPATH . '/wp-load.php');
 include_once(ABSPATH . "wp-includes/pluggable.php");
 include_once(ABSPATH . "wp-admin/includes/user.php");
 
-
-require "config/database.php";
-
-use Illuminate\Database\Capsule\Manager as Capsule;
-use Illuminate\Events\Dispatcher;
-use Illuminate\Container\Container;
-use Illuminate\Http\Request;
-use Illuminate\Pagination\Paginator;
-use Illuminate\View\ViewServiceProvider;
-use Illuminate\Html\HtmlServiceProvider;
-
-
-$capsule = new Capsule;
-
-$db = [
-    'driver' => 'mysql',
-    'host' => DB_HOST,
-    //'port' => DB_PORT,
-    'database' => DB_NAME,
-    'username' => DB_USER,
-    'password' => DB_PASSWORD,
-    'charset' => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-];
-
-$capsule->addConnection($db);
-
-$capsule->setEventDispatcher(new Dispatcher(new Container));
-
-//Set Pagination Variable
-$inputs = Request::capture()->all();
-
-
-
-
-if(isset($inputs['page']))
-{
-    Paginator::currentPageResolver(function () use ($inputs) {
-        return $inputs['page'];
-    });
-}
-
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
-
+// Load admin scripts
+include_once ('bootstrap.php');
 
 // Load admin scripts
 include_once ('config/admin-scripts.php');
